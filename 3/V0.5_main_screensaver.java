d49474a9a00e56e73ce5d11f2a2041ca2ef8e8f0
bruce liu <lightliudark@gmail.com>
2019-10-08 10:58:42 -0600

    Bug on wake mode would save the last animation of the screen saver fixed

---
+++ 3/V0.5_main_screensaver.java
@@ -0,0 +1,249 @@
+input.onGesture(Gesture.LogoDown, function () {
+    while (enable == true) {
+        splash()
+    }
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    while (enable == true) {
+        sweep()
+    }
+})
+function splash () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+}
+function sweep () {
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+    basic.pause(500)
+}
+function wave () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # .
+        . . # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . # .
+        . . # . .
+        . # . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+}
+function rising_tide () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+}
+input.onGesture(Gesture.LogoUp, function () {
+    while (enable == true) {
+        rising_tide()
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    while (enable == true) {
+        wave()
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    while (enable == true) {
+        firework()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.pause(2000)
+    if (input.buttonIsPressed(Button.A)) {
+        enable = true
+        basic.showString("sleep enabled")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.pause(2000)
+    if (input.buttonIsPressed(Button.B)) {
+        enable = false
+        basic.showString("I'm awake, I'm awake")
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+function firework () {
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        # # . # #
+        . # # # .
+        # . # . #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . # . .
+        # . . . #
+        . # . # .
+        `)
+    basic.pause(500)
+}
+let enable = false
+enable = false
